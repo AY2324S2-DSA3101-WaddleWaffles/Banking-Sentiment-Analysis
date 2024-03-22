5ae50e934b15c2f46f5aee767d184a3974ed8909
Eigen-V <e0530215@u.nus.edu>
2024-03-23 01:31:16 +0800

    Sorted grid sizes

--- client/src/ProductReviews.jsx
+++ client/src/ProductReviews.jsx
@@ -1,23 +1,61 @@
 import React from 'react'
-import './index.css'
 import { Container, Grid, SimpleGrid, Skeleton, rem } from '@mantine/core';
-import './styles.css'
-
-const child = <Skeleton height={140} radius="md" animate={false} />;
+// const child = <Skeleton height={140} radius="md" animate={false} />;
+import "./ProductReviews.css"
 
 function ProductReviews() {
   return (
-    <Container  size = "xl">
-      {/* <Skeleton height= '80vh' radius="md" animate={false} /> */}
-      <Grid styles ={{margin: "0"}}>
-        <Grid.Col span={{ base: 12, xs: 4 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 8 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 8 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 4 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 30, xs: 7 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 3 }}>{child}</Grid.Col>
-        <Grid.Col span={{ base: 12, xs: 6 }}>{child}</Grid.Col>
-      </Grid>
+    <Container size="100%" style={{ display: "flex", }}>
+
+
+
+      {/* First Grid - Right Side */}
+      <div style={{ flex: "50%", marginLeft: "20px" }}>
+      <Container style={{padding: "5px", size: "100%" }}>
+        <Grid styles={{ margin: "2" }}>
+
+          <Grid.Col span={{ base: 12, xs: 12}} style={{ height: "500px" }} className='columns'>
+            <Container size="100%">
+                <h2>orginal comment</h2>
+                <p>three columns: comment content, sentiment category, topic</p>
+            </Container>
+          </Grid.Col>
+
+          <Grid.Col span={{ base: 12, xs: 12}} style={{ height: "200px" }} className='columns'>
+            <Container size="100%">
+                <h2> suggested solution </h2>
+                <p>if we have</p>
+            </Container>
+          </Grid.Col>
+
+        </Grid>
+      </Container>
+      </div>
+
+      
+       {/* First Grid - Left Side */}
+       <div style={{ flex: "50%", marginLeft: "20px" }}>
+      <Container style={{padding: "5px", size: "100%" }}>
+        <Grid styles={{ margin: "2" }}>
+
+          <Grid.Col span={{ base: 12, xs: 12}} style={{ height: "350px" }} className='columns'>
+            <Container size="100%">
+                <h2>comparison with other banks</h2>
+                <p>change according to # of banks filtered</p>
+            </Container>
+          </Grid.Col>
+
+          <Grid.Col span={{ base: 12, xs: 12}} style={{ height: "350px" }} className='columns'>
+            <Container size="100%">
+                <h2> word cloud </h2>
+                <p>top~10 frequent words</p>
+            </Container>
+          </Grid.Col>
+
+        </Grid>
+      </Container>
+      </div>
+
     </Container>
   );
 }
