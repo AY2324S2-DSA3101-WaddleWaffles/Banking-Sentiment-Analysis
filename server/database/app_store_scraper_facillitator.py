1a0458b20047caef58e4761402728d9f461f83cf
Lincoln Teo <lincolnteo@Lincolns-Air.lan>
2024-03-28 16:35:49 +0800

    Added scraping pipeline

---
+++ server/database/app_store_scraper_facillitator.py
@@ -0,0 +1,136 @@
+"""
+Scrapes data from the apple store
+If given a parameter, datetime, should only scrape data from that datetime onwards
+"""
+
+import pandas as pd
+import numpy as np
+import json
+import datetime
+from app_store_scraper import AppStore
+
+## Request limit should be around 3500 requests per hour. If no datetime will not have enough requests to scrape.
+
+class AppScraperFacillitator:
+    """
+    A class for scraping the Apple store for reviews
+
+    Attr:
+        bank_names (list): List of all bank names
+        apps (dictionary): Dictionary of bank names to apps
+        app_ids (dictoinary): Dictionary of bank names to app_id
+    """
+
+    def __init__(self, bank_names, apps, app_ids):
+        """
+        Initialise the Facillitator object
+        Args:
+            bank_names (list): List of all bank names
+            apps (dictionary): Dictionary of bank names to apps
+            app_ids (dictoinary): Dictionary of bank names to app_id
+        """
+        self.scraped_data = []
+        self.bank_names = ['gxs', 'dbs', 'ocbc', 'uob','trust','maribank']
+        self.apps = {'gxs': 'gxs-bank','dbs': 'dbs-digibank', 'ocbc': 'ocbc-digital-mobile-banking','uob': 'uob-tmrw','trust': 'trust-bank-sg','maribank': 'maribank'}
+        self.app_ids = {'gxs': '1632183616', 'dbs': '1068403826', 'ocbc': '292506828', 'uob' : '1049286296', 'trust':'1598460384', 'maribank':'1658919834'}
+
+    def scrape_banks(self, datetime_scrape = datetime.min):
+        """
+        Scrapes reviews for multiple banks' apps and adds the bank name to each review.
+
+        Args:
+            datetime (datetime, optional): Scrapes data from datetime onwards, if empty, scrapes all reviews.
+
+        Returns:
+            pd.Dataframe: of all bank reviews scraped
+            datetime: Current datetime
+        """
+
+        reviews = []
+        banks_in_review = []
+        for bank in self.bank_names:
+            result = AppStore(country = 'sg', app_name = self.apps[bank], app_id = self.app_ids[bank], after = datetime_scrape)
+            reviews.append(result)
+            for i in result:
+                banks_in_review.append(bank)
+
+        pd_reviews = pd.DataFrame(np.array(result), columns = ['review'])
+
+        pd_reviews = pd_reviews.join(pd.DataFrame(pd_reviews.pop('review').tolist()))
+
+        pd_reviews["bank"] = banks_in_review
+
+        return pd_reviews, datetime.now()
+
+
+
+
+# GXS = AppStore(country = 'sg', app_name = 'gxs-bank', app_id = '1632183616')
+
+# GXS.review()
+
+# pd_GXS_reviews = pd.DataFrame(np.array(GXS.reviews), columns = ['review'])
+
+# pd_GXS_reviews = pd_GXS_reviews.join(pd.DataFrame(pd_GXS_reviews.pop('review').tolist()))
+
+#print(pd_GXS_reviews)
+
+# pd_GXS_reviews.to_csv('Banking-Sentiment-Analysis/server/database/data/GXS_app_reviews.csv') 
+
+# GXS_review_rating = pd_GXS_reviews[['review', 'rating']]
+# print(GXS_review_rating)
+
+# DBS = AppStore(country = 'sg', app_name = 'dbs-digibank', app_id = '1068403826')
+
+# DBS.review()
+
+# pd_DBS_reviews = pd.DataFrame(np.array(DBS.reviews), columns = ['review'])
+
+# pd_DBS_reviews = pd_DBS_reviews.join(pd.DataFrame(pd_DBS_reviews.pop('review').tolist()))
+
+#print(pd_DBS_reviews)
+
+# pd_DBS_reviews.to_csv('Banking-Sentiment-Analysis/server/database/data/DBS_app_reviews.csv') 
+
+# OCBC = AppStore(country = 'sg', app_name = 'ocbc-digital-mobile-banking', app_id = '292506828')
+
+# OCBC.review()
+
+# pd_OCBC_reviews = pd.DataFrame(np.array(OCBC.reviews), columns = ['review'])
+
+# pd_OCBC_reviews = pd_OCBC_reviews.join(pd.DataFrame(pd_OCBC_reviews.pop('review').tolist()))
+
+# #print(pd_OCBC_reviews)
+
+# pd_OCBC_reviews.to_csv('Banking-Sentiment-Analysis/server/database/data/OCBC_app_reviews.csv') 
+
+# UOB = AppStore(country = 'sg', app_name = 'uob-tmrw', app_id = '1049286296')
+
+# UOB.review()
+
+# pd_UOB_reviews = pd.DataFrame(np.array(UOB.reviews), columns = ['review'])
+
+# pd_UOB_reviews = pd_UOB_reviews.join(pd.DataFrame(pd_UOB_reviews.pop('review').tolist()))
+
+# pd_UOB_reviews.to_csv('Banking-Sentiment-Analysis/server/database/data/UOB_app_reviews.csv') 
+
+# Trust = AppStore(country = 'sg', app_name = 'trust-bank-sg', app_id = '1598460384')
+
+# Trust.review()
+
+# pd_Trust_reviews = pd.DataFrame(np.array(Trust.reviews), columns = ['review'])
+
+# pd_Trust_reviews = pd_Trust_reviews.join(pd.DataFrame(pd_Trust_reviews.pop('review').tolist()))
+
+
+# pd_Trust_reviews.to_csv('Banking-Sentiment-Analysis/server/database/data/Trust_app_reviews.csv') 
+
+# Maribank = AppStore(country = 'sg', app_name = 'maribank', app_id = '1658919834')
+
+# Maribank.review()
+
+# pd_Maribank_reviews = pd.DataFrame(np.array(Maribank.reviews), columns = ['review'])
+
+# pd_Maribank_reviews = pd_Maribank_reviews.join(pd.DataFrame(pd_Maribank_reviews.pop('review').tolist()))
+
+# pd_Maribank_reviews.to_csv('Banking-Sentiment-Analysis/server/database/data/Maribank_app_reviews.csv') 
---
+++ server/database/play_store_scraper_facillitator.py
@@ -0,0 +1,184 @@
+"""
+Scrapes data from the google plays store
+If given a parameter, continuation token, should only scrape data from that review onwards
+"""
+
+import pandas as pd
+import numpy as np
+import json
+from google_play_scraper import reviews, Sort, app
+
+# Quota is 200,000 queries per day
+
+class PlayStoreScraperFacillitator:
+    """
+    A class for scraping the google play store for reviews
+
+    Attr:
+        bank_names (list): List of all bank names
+        apps (dictionary): Dictionary of bank names to apps
+    """
+
+    def __init__(self, bank_names, apps):
+        """
+        Initialise playstore Facillitator object
+        Args:
+            bank_names (list): List of all bank names
+            apps (dictionary): Dictionary of bank names to apps
+        """
+
+        self.scraped_data = []
+        self.bank_names = ['gxs', 'dbs', 'ocbc', 'uob','trust','maribank']
+        self.apps = {'gxs': 'sg.com.gxs.app','dbs': 'com.dbs.sg.dbsmbanking', 'ocbc': 'com.ocbc.mobile','uob': 'com.uob.mighty.app','trust': 'sg.trust','maribank': 'sg.com.maribankmobile.digitalbank'}
+
+    def scrape_banks(self, tokens):
+        """
+        Scrapes reviews for multiple banks' apps and adds the bank name to each review.
+
+        Args:
+            continuation_token (token, optional): Token allowing the scraper to skip past scraped data.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the scraped dataset with columns for reviews and bank names.
+            dict: A dictionary mapping bank names to continuation tokens.
+        """
+        reviews = []
+        banks_in_review = []
+        for bank in self.bank_names:
+            result, continuation_token = reviews(
+                self.apps[bank],
+                lang = 'en',
+                country = 'us',
+                sort = Sort.NEWEST,
+                count = 5000,
+                continuation_token = tokens
+            )
+            tokens[bank] = continuation_token
+            reviews.append(result)
+            for i in result:
+                banks_in_review.append(bank)
+
+        pd_reviews = pd.DataFrame(np.array(result), columns = ['review'])
+
+        pd_reviews = pd_reviews.join(pd.DataFrame(pd_reviews.pop('review').tolist()))
+
+        pd_reviews["bank"] = banks_in_review
+
+        return pd_reviews, tokens
+
+# def scrape_dbs(token = None):
+#     """
+#     Scrapes DBS bank app for reviews
+#     Args:
+#         continuation_token (token, optional) : Token allows scraper to skip past scraped data
+#     """
+#     DBS_result, dbs_continuation_token = reviews(
+#         'com.dbs.sg.dbsmbanking',
+#         lang = 'en',
+#         country = 'us',
+#         sort = Sort.NEWEST,
+#         count = 5000,
+#         continuation_token = token
+#     )
+
+#     pd_DBS_reviews = pd.DataFrame(np.array(DBS_result), columns = ['review'])
+
+#     pd_DBS_reviews = pd_DBS_reviews.join(pd.DataFrame(pd_DBS_reviews.pop('review').tolist()))
+
+#     pd_DBS_reviews["bank"] = "dbs"
+
+#     return pd_DBS_reviews
+
+# def scrape_ocbc(token = None):
+#     """
+#     Scrapes OCBC bank app for reviews
+#     Args:
+#         continuation_token (token, optional) : Token allows scraper to skip past scraped data
+#     """
+#     ocbc_result, ocbc_continuation_token = reviews(
+#         'com.ocbc.mobile',
+#         lang = 'en',
+#         country = 'us',
+#         sort = Sort.NEWEST,
+#         count = 5000,
+#         continuation_token = token
+#     )
+
+#     pd_ocbc_reviews = pd.DataFrame(np.array(ocbc_result), columns = ['review'])
+
+#     pd_ocbc_reviews = pd_ocbc_reviews.join(pd.DataFrame(pd_ocbc_reviews.pop('review').tolist()))
+
+#     pd_ocbc_reviews["bank"] = "ocbc"
+
+#     return pd_ocbc_reviews
+#     # pd_ocbc_reviews.to_csv('play_store_data/ocbc_playstore_reviews.csv') 
+
+# def scrape_uob(token = None):
+#     """
+#     Scrapes UOB bank app for reviews
+#     Args:
+#         continuation_token (token, optional) : Token allows scraper to skip past scraped data
+#     """
+#     uob_result, uob_continuation_token = reviews(
+#         'com.uob.mighty.app',
+#         lang = 'en',
+#         country = 'us',
+#         sort = Sort.NEWEST,
+#         count = 5000,
+#         continuation_token = token
+#     )
+
+#     pd_uob_reviews = pd.DataFrame(np.array(uob_result), columns = ['review'])
+
+#     pd_uob_reviews = pd_uob_reviews.join(pd.DataFrame(pd_uob_reviews.pop('review').tolist()))
+
+#     pd_uob_reviews["bank"] = "uob"
+
+#     return pd_uob_reviews
+
+# def scrape_trust(token = None):
+#     """
+#     Scrapes trust bank app for reviews
+#     Args:
+#         continuation_token (token, optional) : Token allows scraper to skip past scraped data
+#     """
+#     trust_result, trust_continuation_token = reviews(
+#         'sg.trust',
+#         lang = 'en',
+#         country = 'us',
+#         sort = Sort.NEWEST,
+#         count = 5000,
+#         continuation_token = token
+#     )
+
+#     pd_trust_reviews = pd.DataFrame(np.array(trust_result), columns = ['review'])
+
+#     pd_trust_reviews = pd_trust_reviews.join(pd.DataFrame(pd_trust_reviews.pop('review').tolist()))
+
+#     pd_trust_reviews["bank"] = "trust"
+
+#     return pd_trust_reviews
+
+# def scrape_maribank(token = None):
+#     """
+#     Scrapes DBS bank app for reviews
+#     Args:
+#         continuation_token (token, optional) : Token allows scraper to skip past scraped data
+#     """
+#     maribank_result, maribank_continuation_token = reviews(
+#         'sg.com.maribankmobile.digitalbank',
+#         lang = 'en',
+#         country = 'us',
+#         sort = Sort.NEWEST,
+#         count = 5000,
+#         continuation_token = token
+#     )
+
+#     pd_maribank_reviews = pd.DataFrame(np.array(maribank_result), columns = ['review'])
+
+#     pd_maribank_reviews = pd_maribank_reviews.join(pd.DataFrame(pd_maribank_reviews.pop('review').tolist()))
+
+#     pd_maribank_reviews["bank"] = "maribank"
+
+#     return pd_maribank_reviews
+#     # Was empty, still included since we did it for apple
