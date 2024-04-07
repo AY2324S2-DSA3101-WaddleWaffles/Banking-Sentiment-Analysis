2079aa110977591e076f4d8c896ff316756a2a78
colesllaw <nicole.chong2002@gmail.com>
2024-04-07 15:34:16 +0800

    Clear Overview.jsx for incoming changes to avoid conflict

--- client/src/Overview.jsx
+++ client/src/Overview.jsx
@@ -10,36 +10,3 @@ import RefreshDatabase from "./components/RefreshButton";
 import SentimentByTopic from "./components/BarChart";
 
 //import PickDateRange from "./components/DateFilter";
-
-export default function Overview({ selectedDateRange }) {
-
-  return (
-    <Container size="100%">
-        {/* <div style = {{ marginLeft: '1000px', height: "250px" ,marginTop: "10px"}}>
-            <RefreshDatabase />
-        </div> */}
-
-        {/* <div> 
-            <DonutChartComponent />
-        </div> */}
-
-        {/* <div style = {{ marginLeft: '500px', height: "250px" ,marginTop: "-250px"}}> 
-            <GetNumReviews /> reviews 
-        </div>  */}
-
-        <div> 
-            <OverallGXSBySentiment selectedDateRange={selectedDateRange}/>
-        </div> 
-
-
-
-        {/* <div >
-            <SentimentByTopic />
-        </div> */}
-
-        {/* <div style = {{ marginTop: '90px' , marginLeft: '-600px' }}>
-            <TimeSeriesGXS />
-        </div> */}
-    </Container>
-  );
-}
